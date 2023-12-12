d13201f80a7575010a2761ccab0095db82c78d7b
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-12-12 23:25:35 +0700

    fix cors

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// disable CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
 app.use('/', indexRouter);
 
 module.exports = app;
