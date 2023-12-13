b8f17a60e514070a721f23c2c280140a55c1d2a4
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-12-13 15:01:11 +0700

    show error instead of exit #3

--- database/answers.js
+++ database/answers.js
@@ -4,21 +4,21 @@ const getAnswersByQuestionId = async (question_id) => {
     const text = `SELECT *
                   FROM answers
                   WHERE questionid = ${question_id}`;
-    return (await query(text)).rows;
+    return (await query(text))?.rows;
 }
 
 const findById = async (id) => {
     const text = `SELECT *
                   FROM answers
                   WHERE id = ${id}`;
-    return (await query(text)).rows[0];
+    return (await query(text))?.rows[0];
 }
 
 const createAnswer = async (question_id, content, is_anonymous, user_id) => {
     const text = `INSERT INTO answers (questionid, content, isanonymous, userid)
                   VALUES ('${question_id}', '${content}', ${is_anonymous}, ${user_id})
                   RETURNING *`;
-    return (await query(text)).rows[0];
+    return (await query(text))?.rows[0];
 }
 
 module.exports = {
