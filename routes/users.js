0757f94e7bd7545259b584c2a42f51ed51aea83a
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-12-19 23:49:04 +0700

    get user info

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 let express = require('express');
+const controllers = require('../controllers');
 let router = express.Router();
 
+router.get('/:id', controllers.users.findUserById);
 module.exports = router;
